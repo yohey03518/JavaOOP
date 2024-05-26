a1877b4110a52c60d47a426de5bae99243f826e6
erwin <yohey03518@gmail.com>
2024-05-26 23:41:37 +0800

    fix build error

--- src/main/java/factorPattern/otp/OtpFactory.java
+++ src/main/java/factorPattern/otp/OtpFactory.java
@@ -3,7 +3,7 @@ package factorPattern.otp;
 public class OtpFactory {
     public BaseOtp Create(User user, String otpType) {
         return switch (otpType) {
-            case "email" -> new EmailOtp(user);
+            case "email" -> new EmailOtp(user, new EmailSmtpProvider("10.12.50.11", "25", "otpUser", "1234qwer"));
             case "sms" -> new SmsOtp(user, new TwilioSmsProvider("https://api.twilio.com", "67290H!@*8798==", "dh124as"));
             // app
             default -> throw new IllegalArgumentException("Invalid otp type");
